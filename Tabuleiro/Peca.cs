000e8ebb3a9c21e1e5a20020c606c345e73a8a16
Bruno Machado <nicolau.cortella@gmail.com>
2019-07-09 17:29:14 -0300

    Initial test with Moviments

--- Tabuleiro/Peca.cs
+++ Tabuleiro/Peca.cs
@@ -1,5 +1,5 @@
 ﻿namespace tabuleiro {
-    class Peca
+    abstract class Peca
     {
 
         public Posicao posicao { get; set; }
@@ -15,6 +15,8 @@
             this.qteMovimentos = 0;
         }
 
+        public abstract bool[,] movimentosPossiveis();
+       
         public void incrementarQteMovimentos()
         {
             qteMovimentos++;
